006a63d4de4c07cad57fbf60fe96fdfd2ef40efe
shivammaniharsahu <shivam.raipur1234@gmail.com>
2021-04-13 12:35:36 +0530

    adding create user page and transaction history page

---
+++ createuser.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Create User</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/table.css">
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+    <link rel="stylesheet" type="text/css" href="css/createuser.css">
+    
+<link  rel="stylesheet" type="text/css" href="css/navbar.css">
+</head>
+
+<body style="background-color: #e67e22">
+
+<?php
+    include 'config.php';
+    if(isset($_POST['submit'])){
+    
+    $name=$_POST['name'];
+    $email=$_POST['email'];
+    $balance=$_POST['balance'];
+    $sql="insert into users(name,email,balance) values('{$name}','{$email}','{$balance}')";
+    $result=mysqli_query($conn,$sql);
+    if($result){
+               echo "<script> alert('Hurray! User created');
+                               window.location='transfermoney.php';
+                     </script>";
+                    
+    }
+  }
+?>
+
+
+<div class="formDiv">
+        <h1 class="title">Create a User</h1>
+        <form action="login.html">
+            <div class="input-group">
+                <i class="fas fa-user"></i>
+                <input type="text" name="name" placeholder="Name" required>
+            </div>
+            <div class="input-group">
+            <i class="fas fa-envelope"></i>
+                <input type="email" name="email" placeholder="Email" required>
+            </div>
+            
+            <div class="input-group">
+            <i class="fas fa-money-check"></i>
+                <input type="number" name="balance" placeholder="Balance" required>
+            </div>
+            <button type="submit" value="CREATE" name="submit" class="btn">Create</button>
+        </form>
+    </div>
+
+<script src="https://kit.fontawesome.com/4077c6ef6a.js" crossorigin="anonymous"></script>
+<!-- 
+  <div class="background">
+  <div class="container">
+    <div class="screen">
+      <div class="screen-header">
+        <div class="screen-header-right">
+          <div class="screen-header-ellipsis"></div>
+          <div class="screen-header-ellipsis"></div>
+          <div class="screen-header-ellipsis"></div>
+        </div>
+      </div>
+      <div class="screen-body">
+        <div class="screen-body-item left">
+          <img class="img-fluid" src="img/user3.jpg" style="border: none; border-radius: 50%;">
+        </div>
+        <div class="screen-body-item">
+          <form class="app-form" method="post">
+            <div class="app-form-group">
+              <input class="app-form-control" placeholder="NAME" type="text" name="name" required>
+            </div>
+            <div class="app-form-group">
+              <input class="app-form-control" placeholder="EMAIL" type="email" name="email" required>
+            </div>
+            <div class="app-form-group">
+              <input class="app-form-control" placeholder="BALANCE" type="number" name="balance" required>
+            </div>
+            <br>
+            <div class="app-form-group button">
+              <input class="app-form-button" type="submit" value="CREATE" name="submit"></input>
+              <input class="app-form-button" type="reset" value="RESET" name="reset"></input>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</div> -->
+
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'>
+<script type="text/javascript" src="js/navbar.js"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+</body>
+</html>
