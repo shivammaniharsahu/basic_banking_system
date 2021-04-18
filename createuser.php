2a79a7531fbf8873c25aadb4e672d17f7fcfe302
shivam manihar sahu <48375036+shivammaniharsahu@users.noreply.github.com>
2021-04-18 16:13:30 +0530

    Add files via upload

--- createuser.php
+++ createuser.php
@@ -6,14 +6,17 @@
     <title>Create User</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
     <link rel="stylesheet" type="text/css" href="css/createuser.css">
     
+    <link rel="stylesheet" type="text/css" href="css/bg.css">
     <link rel="stylesheet" type="text/css" href="css/navbar.css">
 </head>
 <body >
 <!-- navbar -->
   
+<div class="bg"></div>
+<div class="bg bg2"></div>
+<div class="bg bg3"></div>
 
 <div class="topnav" id="myTopnav">
   <a href="index.php" style="text-shadow: 0 0 3px #FF0000, 0 0 5px #0000FF;"  class="active">The Spark Bank</a>
